360d7e79eba8aa372a593b3b2e7d73a4ebd21d20
SomAndr92 <nikmos92@mail.ru>
2023-09-16 13:15:06 +0300

    v2

--- script.js
+++ script.js
@@ -11,15 +11,44 @@ $(document).ready(function () {
 
     $(".btn").on("click", function () {
         /*alert("привет")*/
+        if ($("#num").val() == "") {
+            alert("Введите номер карты")
+        } else {
+            if ($("#MM").val() == "") {
+                alert("Введите месяц")
+            } else {
+                if ($("#YY").val() == "") {
+                    alert("Введите год")
+                } else {
+                    if ($("#CCV").val() == "") {
+                        alert("Введите CCV")
+                    } else {
+                        if ($("#num").val() == "") {
+                            alert("Введите Имя держателя карты")
+                        } else {
+                            console.log("Номер карты:" + $("#name").val())
+                            console.log("Срок действия карты:" + $("#MM").val() + "/" + $("#YY").val())
+                        }
+                    }
 
-        $(".tt").toggle(300);
-        $("div").addClass("hehe");
-        console.log($(".tt").val())
-    })
-    $(".tt").on("change", function () {
-        console.log($(this).val())
-    })
-    $(".btn1").on("click", function () {
-        $("div").addClass("bac");
+                }
+            }
+        }
     })
 })
+
+
+/*
+        $(".tt").toggle(300);
+        $("div").addClass("hehe");
+        console.log($(".tt").val())*/
+
+/*
+$(".tt").on("change", function () {
+    console.log($(this).val())
+})
+$(".btn1").on("click", function () {
+    $("div").addClass("bac");
+})*/
+
+
